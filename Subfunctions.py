84217f91dec7075886e61b3e080da5c4ad7c9be3
Ayanmuhammad12 <125782712+Ayanmuhammad12@users.noreply.github.com>
2026-09-17 12:09:31 -0500

    updating funciton skelaton

--- Subfunctions.py
+++ Subfunctions.py
@@ -1,1 +1,36 @@
 #subfunctions 
+
+def get_mass():
+    #Computes total mass of rover
+    return
+
+def get_gear_ratio():
+    #Returns the speed reduction ratio
+    return
+
+
+def tau_dcmotor():
+    #Returns the motor shaft torque given shaft speed and motor specs
+    return
+
+
+def F_drive():
+    #Returns the force applied to the rover by the drive system given drive system and shaft speed
+    return
+ 
+
+def F_gravity():
+    #magnitude of the force component acting on the rover in the direction of its
+#translational motion due to gravity as a function of terrain inclination angle and rover
+#properties
+    return
+
+def F_rolling():
+    #magnitude of force component due to rolling resistances given the terrain inclination angle, rover properties, and a
+#rolling resistance coefficient
+
+    return
+
+def F_net():
+    # magnitude of net force acting on the rover
+    return
