0f23bc3ff95a53bb0dcd60777a20b288f8acf589
Ayanmuhammad12 <125782712+Ayanmuhammad12@users.noreply.github.com>
2026-09-19 13:26:14 -0500

    added get_mass

--- Subfunctions.py
+++ Subfunctions.py
@@ -1,10 +1,60 @@
 #subfunctions 
 ### SIGN CONVENTION: POSITIVE (FORWARD OR DOWNHILL) ; NEGATIVE (BACKWARD OR UPHILL)###
+import numpy as np
+from math import erf
 
-def get_mass():
+#defining rover dict for testing
+# --- Rover component dicts ---
+
+motor = {
+    'torque_stall':  170,    # Nm
+    'torque_noload':   0,    # Nm
+    'speed_noload':  3.80,   # rad/s
+    'mass':          5.0     # kg
+}
+wheel = {
+    'radius': 0.30,   # m
+    'mass':   1.0     # kg
+}
+speed_reducer = {
+    'type':        'reverted',
+    'diam_pinion': 0.04,   # m
+    'diam_gear':   0.07,   # m
+    'mass':        1.5     # kg
+}
+wheel_assembly = {
+    'wheel':         wheel,
+    'speed_reducer': speed_reducer,
+    'motor':         motor
+}
+chassis         = {'mass': 659}   # kg
+science_payload = {'mass':  75}   # kg
+power_subsys    = {'mass':  90}   # kg
+rover = {
+    'wheel_assembly':  wheel_assembly,
+    'chassis':         chassis,
+    'science_payload': science_payload,
+    'power_subsys':    power_subsys
+}
+# --- Planet dict (separate from rover) ---
+planet = {'g': 3.72}   # m/s^2, Mars gravity
+
+
+def get_mass(rover):
     #Computes total mass of rover
-    return
+    if type(rover) != dict:
+        raise Exception('Input rover must be a dict')
+    
+    #finding mass
+    chassis_mass = rover['chassis']['mass']
+    wheel_mass = rover['wheel_assembly']['wheel']['mass']
+    motor_mass = rover['wheel_assembly']['motor']['mass']
+    payload_mass = rover['science_payload']['mass']
+    reducer_mass = rover['wheel_assembly']['speed_reducer']['mass']
+    power_mass = rover['power_subsys']['mass']
+    m = chassis_mass + payload_mass + power_mass + (6 * (wheel_mass + motor_mass + reducer_mass))
 
+    return m
 
 
 
@@ -38,3 +88,6 @@ def F_rolling():
 def F_net():
     # magnitude of net force acting on the rover
     return
+
+
+print(get_mass(rover))
