283640b904ec0f75a60197bfb3464b68842dcc23
Ayanmuhammad12 <125782712+Ayanmuhammad12@users.noreply.github.com>
2026-09-20 13:45:46 -0500

    Fix exception messages in subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -62,7 +62,7 @@ def tau_dcmotor(omega,motor):
     #torque for array
     tau = tau_stall - (slope * omega)
     tau[omega < 0] = tau_stall
-    tau [omega > omega_noload] = 0
+    tau[omega > omega_noload] = 0
 
     return tau
 
@@ -115,9 +115,9 @@ def F_rolling(omega, terrain_angle, rover, planet, Crr):
     if np.any(np.array(terrain_angle) > 75) or  np.any(np.array(terrain_angle) < -75):
         raise Exception('F_rolling - terrain angle must be between -75 and 75 degrees')
     if type(rover) != dict:
-        raise Exception('F-rolling - Input rover must be a dict')
+        raise Exception('F_rolling - Input rover must be a dict')
     if type(planet) != dict:
-            raise Exception('F-rolling - Input planet must be a dict')
+            raise Exception('F_rolling - Input planet must be a dict')
     if not (isinstance(Crr, (int, float)) and Crr > 0):
         raise Exception('F_rolling - Crr must be a positive scalar')
     
@@ -139,17 +139,17 @@ def F_rolling(omega, terrain_angle, rover, planet, Crr):
 def F_net(omega, terrain_angle, rover, planet, Crr):
     # magnitude of net force acting on the rover
     if not isinstance(omega, (float, int, np.ndarray)):
-        raise Exception('F-net - Omega must be a scalar or numpy array')
+        raise Exception('F_net - Omega must be a scalar or numpy array')
     if not isinstance(terrain_angle,(float, int, np.ndarray)):
-        raise Exception('F-net - terrain angle must be a scalar or numpy array')
+        raise Exception('F_net - terrain angle must be a scalar or numpy array')
     if np.shape(np.array(omega)) != np.shape(np.array(terrain_angle)):
-        raise Exception('F-net - omega and terrain angle must have the same shape')
+        raise Exception('F_net - omega and terrain angle must have the same shape')
     if np.any(np.array(terrain_angle) > 75) or  np.any(np.array(terrain_angle) < -75):
-        raise Exception('F-net - terrain angle must be between -75 and 75 degrees')
+        raise Exception('F_net - terrain angle must be between -75 and 75 degrees')
     if type(rover) != dict:
-        raise Exception('F-net - Input rover must be a dict')
+        raise Exception('F_net - Input rover must be a dict')
     if type(planet) != dict:
-        raise Exception('F-net - Input planet must be a dict')
+        raise Exception('F_net - Input planet must be a dict')
     if not (isinstance(Crr, (int, float)) and Crr > 0):
         raise Exception('F_net - Crr must be a positive scalar')
 
